e25eed94bb0b5cc02b6dba0c206da5e1c9c6f866
MdFarzan <mdfarzan776@gmail.com>
2020-11-18 15:59:57 +0530

    Few class added and changed for easy styling, basic number and opcode functionality added.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 console.log('Js linked.');
+
+var entry = document.querySelector('#entry');
+var early_evaluation = document.querySelector('#early-evaluation');
+var btns = document.querySelectorAll('.btn');
+var reset = true;
+var result = 0;
+for(let btn of btns){
+    btn.addEventListener('click',(event)=>{
+        
+        entry.innerText += event.target.innerText;
+        
+        //disabling early evaluation if entry containes opcode and dot
+        let v = entry.innerText.charAt(entry.innerText.length-1);
+        if(v!='+' && v!='-' && v!='*' && v!='/' && v!='.')
+        early_evaluation.innerText = eval(entry.innerText);
+        
+
+        
+
+    });
+}
+
+
+
